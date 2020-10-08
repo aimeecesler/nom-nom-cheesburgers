cf2c31fe1555011d9a8a68fcf4bd93047a34a1ab
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-08 16:09:41 -0400

    added starter seeds for mysql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ("Bacon Cheesburger");
+INSERT INTO burgers (name) VALUES ("Double BBQ Brisket Burger");
+INSERT INTO burgers (name) VALUES ("Black Bean Burger");
+INSERT INTO burgers (name) VALUES ("Thanksgiving Turkey Burger");
+INSERT INTO burgers (name, devoured) VALUES ("Southwestern Cheeseburger with Avocado", true);
